e91835581260ec93c789efbb98610a255ad3ca21
fahmizainal17 <fahmizainal9@gmail.com>
2025-04-25 12:47:19 +0800

    feat:added files

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
